824044a6322242e9b5bee1b946979cb2bfd11fd4
David <thedaviddelta@gmail.com>
2019-12-02 20:12:08 +0100

    i18n with DB persistence

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
 # SpeedwaBot
-Simple bot de Discord sobre Jojo's hecho con NodeJS.
+<p>
+  <a href="javascript:void(0)">
+    <img alt="Version" src="https://img.shields.io/badge/version-2.0.0-blue.svg?cacheSeconds=2592000" />
+  </a>
+  <a href="./LICENSE">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
 
-## Funcionamiento
-`!jojo help` :
+> Simple Discord bot about Jojo's built on NodeJS
 
-> SpeedwaBot por [@TheDavidDelta]()  
-> Escribe **!jojo** para recibir la imagen de un jojo aleatorio (seasons 1-8).  
-> También incluye a Gyro Zeppeli y algún *easter egg*.  
+#### 🤖 [Add it to your server](https://bots.thedaviddelta.com/SpeedwaBot)
+#### 👷 [The link doesn't work? Try this one!](https://discordapp.com/oauth2/authorize?client_id=644640871400210464&scope=bot&permissions=10240)
 
-## Añádelo a tu server
-Para añadirlo a tu server simplemente usa [este link](http://bots.thedaviddelta.com/SpeedwaBot) ([mirror](https://discordapp.com/oauth2/authorize?client_id=644640871400210464&scope=bot&permissions=10240)).
+
+## ✨ How does it work?
+
+Just write `!jojo` and the bot will reply an image of a random jojo (seasons 1-8).  
+It also includes Gyro Zeppeli and some *easter eggs*.
+
+
+## 🌍 Internationalization
+
+Currently, this bot is supported in two languages:
++ **en**: English (*default*)
++ **es**: Spanish
+
+To select the language just write `!jojo lang` followed by one of the above listed language codes.
+
+If you'd like to contribute with a translation to your own language, send an email to [speedwabot@thedaviddelta.com](mailto:speedwabot@thedaviddelta.com) or [open an issue](https://github.com/TheDavidDelta/SpeedwaBot/issues) with the [needed translations](./i18n.json).
+
+
+## 📝 License
+
+Copyright © 2019 [TheDavidDelta](https://github.com/TheDavidDelta).  
+This project is [MIT](./LICENSE) licensed.
--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
 
+const i18n = require('./i18n.json');
 const files = fs.readdirSync(`${__dirname}/img/`).map(path => new Discord.Attachment(`${__dirname}/img/${path}`, path));
 
+const url = process.env.MONGODB_URI;
+const conOptions = { useUnifiedTopology: true, useNewUrlParser: true };
+
+// TODO: top-level await still not working
+let languages = [];
+MongoClient.connect(url, conOptions).then(client => 
+    client.db().collection("language").find().toArray()
+        .then(res => languages = res)
+        .catch(err => console.error("ERROR: ", err))
+        .finally(client.close())
+).catch(err => console.error("ERROR: ", err));
+
 client.on('ready', () => {
-    console.log(`Logueado como ${client.user.tag}!`);
-    client.user.setActivity("!jojo")
+    client.user.setActivity("!jojo");
+    console.log(`Logged in as ${client.user.tag}`);
+});
+
+client.on('guildCreate', guild => {
+    // TODO: optional chaining still not implemented
+    const objLang = languages.find(e => e._id === +guild.id);
+    const lang = objLang ? objLang.lang : "en";
+
+    guild.channels.find(ch => ch.type === "text").send(i18n[lang].help);
 });
 
 client.on('message', msg => {
@@ -18,45 +40,38 @@ client.on('message', msg => {
     if (!lower.startsWith("!jojo")) 
         return;
     
+    const id = msg.guild ? +msg.guild.id : +msg.author.id;
+
+    // TODO: optional chaining still not implemented
+    const objLang = languages.find(e => e._id === id);
+    const lang = objLang ? objLang.lang : "en";
+
     if (lower === "!jojo help") 
-        return msg.reply(
-            "SpeedwaBot por <@198493822781620224>\n" +
-            "Escribe **!jojo** para recibir la imagen de un jojo aleatorio (seasons 1-8).\n" +
-            "También incluye a Gyro Zeppeli y algún *easter egg*."
-        );
+        return msg.reply(i18n[lang].help);
     
+    if (lower.startsWith("!jojo lang")) {
+        const newLang = lower.slice(11);
+        if (!i18n.hasOwnProperty(newLang))
+            return msg.reply(i18n[lang].langerror);
+        
+        MongoClient.connect(url, conOptions).then(client => 
+            client.db().collection("language").updateOne({ _id: id }, {$set: { lang: newLang }}, { upsert: true })
+                .catch(err => console.error("ERROR: ", err))
+                .finally(client.close())
+        ).catch(err => console.error("ERROR: ", err));
+
+        objLang ? objLang.lang = newLang : languages.push({ _id: +id, lang: newLang });
+        
+        return msg.reply(i18n[newLang].langsuccess);
+    }
+
     const file = files[Math.floor(Math.random() * files.length)];
-    msg.reply(getName(file.name), file);
+
+    // remove numbers (if present) and file extension
+    const name = file.name.replace(/\d*\.(png|jpg)/, "");
+    const replyText = i18n[lang][name] || i18n["en"][name];
+
+    msg.reply(replyText, file);
 });
 
 client.login(process.env.BOT_TOKEN);
-
-const getName = (path) => {
-    // remove numbers (if present) and file extension
-    switch (path.replace(/\d*\.(png|jpg)/, "")) {
-        case "jonathan":
-            return "Jonathan Joestar";
-        case "joseph":
-            return "Joseph Joestar";
-        case "jotaro":
-            return "Jotaro Kujo";
-        case "josuke":
-            return "Josuke Higashikata";
-        case "giorno":
-            return "Giorno Giovana";
-        case "jolyne":
-            return "Jolyne Kujo";
-        case "gyro":
-            return "Gyro Zeppeli";
-        case "johnny":
-            return "Johnny Joestar";
-        case "gappy":
-            return "Gappy Higashikata";
-        case "dio":
-            return "Esperabas un Jojo, pero era yo, Dio!";
-        case "speedwagon":
-            return "Yo no estaba hecho para este tipo de trabajos";
-        default:
-            return "";
-    }
-}
